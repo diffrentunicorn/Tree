841a83b1467db8c727fb63a3ecfb47d237f1b3e1
Ayush Kumar <150270238+diffrentunicorn@users.noreply.github.com>
2024-02-10 13:41:22 +0530

    Update BinaryTreeInorderTraversal.java

--- BinaryTreeInorderTraversal.java
+++ BinaryTreeInorderTraversal.java
@@ -10,16 +10,14 @@ import javax.swing.tree.TreeNode;
  
 
 Example 1:
-
-
 Input: root = [1,null,2,3]
 Output: [1,3,2]
-Example 2:
 
+Example 2:
 Input: root = []
 Output: []
-Example 3:
 
+Example 3:
 Input: root = [1]
 Output: [1]
  
